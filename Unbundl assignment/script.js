3e2a2efce512c16a23d2157942f817938b833826
Narasimha Raju <141636035+Rajunanu@users.noreply.github.com>
2025-07-01 17:00:36 +0530

    Add files via upload

---
+++ Unbundl assignment/script.js
@@ -0,0 +1,42 @@
+let fst = document.getElementById("fst");
+let snd = document.getElementById("snd");
+let trd = document.getElementById("trd");
+let frth = document.getElementById("frth");
+let fifth = document.getElementById("fifth");
+
+
+function fstBtn() {
+    if (fst.style.display === "none") {
+        fst.style.display = "block";
+    } else {
+        fst.style.display = "none";
+    }
+}
+function sndBtn() {
+    if (snd.style.display === "none") {
+        snd.style.display = "block";
+    } else {
+        snd.style.display = "none";
+    }
+}
+function trdBtn() {
+    if (trd.style.display === "none") {
+        trd.style.display = "block";
+    } else {
+        trd.style.display = "none";
+    }
+}
+function frthBtn() {
+    if (frth.style.display === "none") {
+        frth.style.display = "block";
+    } else {
+        frth.style.display = "none";
+    }
+}
+function fifthBtn() {
+    if (fifth.style.display === "none") {
+        fifth.style.display = "block";
+    } else {
+        fifth.style.display = "none";
+    }
+}
